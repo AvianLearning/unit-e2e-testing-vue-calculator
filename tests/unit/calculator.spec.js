f478b7c67b8a898422bd47bd67d5d13f0ad89964
AvianLearning <iaradcliffe@hotmail.com>
2020-10-21 19:56:51 +0100

    First test of adding passing

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -8,5 +8,15 @@ describe('App.vue', () => {
     wrapper.vm.previousTotal = 4
     wrapper.vm.add('5');
     expect(wrapper.vm.runningTotal).to.equal(9)
-  })
+  });
+
+  it('adding numbers returns correct total', () => {
+    const wrapper = shallowMount(App);
+    wrapper.vm.previousTotal = 1;
+    wrapper.vm.add('4');
+    expect(wrapper.vm.runningTotal).to.equal(5);
+  });
+
+
+
 })
